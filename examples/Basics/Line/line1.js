9b831f59a6d3dd4b1db0ac3b8fbe70b7be5bc92e
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-24 15:28:04 +0530

    ex2 modify

--- examples/Basics/Line/line1.js
+++ examples/Basics/Line/line1.js
@@ -3,7 +3,7 @@ var canvas = new Percept.Canvas(document.getElementById('ex2R'));
 var rectangle1 = new Percept.View.Rectangle('rect1', new Percept.Vector2(canvas.width / 2, canvas.height / 2), 15, 15, {
     fill: true, fillColor: 'red'
 });
-var rectangle2 = new Percept.View.Rectangle('rect1', rectangle1.position.add(100, 0), 15, 15, {
+var rectangle2 = new Percept.View.Rectangle('rect2', rectangle1.position.add(100, 0), 15, 15, {
     fill: true, fillColor: 'green'
 });
 
@@ -13,7 +13,7 @@ var line = new Percept.View.Line('myLine', rectangle1, rectangle2, 0, {
 });
 
 var drawing = new Percept.Drawing(canvas, () => {
-    rectangle2.position.addInPlace(0, .2);
+    rectangle2.rotation += .2;
 });
 
 drawing.add(rectangle1);
