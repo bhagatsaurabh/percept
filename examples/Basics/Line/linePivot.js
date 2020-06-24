f4eb612c6200b78e95d9e1884e426e10498b0cd9
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-24 18:58:24 +0530

    ex3 modify

--- examples/Basics/Line/linePivot.js
+++ examples/Basics/Line/linePivot.js
@@ -1,12 +1,20 @@
 var canvas = new Percept.Canvas(document.getElementById('ex3R'));
 
-var line1 = new Percept.View.Line('line1', new Percept.Vector2(canvas.width * .3, canvas.height / 2), new Percept.Vector2(canvas.width * .4, canvas.height / 2), 0, {
-    color: 'red'
-});
+var line1 = new Percept.View.Line(
+    'line1',
+    new Percept.Vector2(canvas.width * .3, canvas.height / 2),
+    new Percept.Vector2(canvas.width * .4, canvas.height / 2),
+    0,
+    { color: 'red'}
+);
 
-var line2 = new Percept.View.Line('line2', new Percept.Vector2(canvas.width * .7, canvas.height / 2), new Percept.Vector2(canvas.width * .8, canvas.height / 2), 0.5, {
-    color: 'green'
-});
+var line2 = new Percept.View.Line(
+    'line2',
+    new Percept.Vector2(canvas.width * .7, canvas.height / 2),
+    new Percept.Vector2(canvas.width * .8, canvas.height / 2),
+    0.5,
+    { color: 'green'}
+);
 
 var drawing = new Percept.Drawing(canvas, () => {
     line1.localRotation += .5;
