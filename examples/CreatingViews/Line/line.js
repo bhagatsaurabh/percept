8d93fd2fe702b7377d88dcc129368d131d5e3129
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-24 23:23:54 +0530

    ex1 modify

--- examples/CreatingViews/Line/line.js
+++ examples/CreatingViews/Line/line.js
@@ -1,16 +1,17 @@
+var canvas = new Percept.Canvas(document.getElementById('canvas'));
+
 var line = new Percept.View.Line(
     'myLine',
-    new Percept.Vector2(100, 100),
-    new Percept.Vector2(200, 200),
+    new Percept.Vector2(canvas.width / 2, canvas.height / 2),
+    new Percept.Vector2(canvas.width / 2 + 100, canvas.height / 2 + 100),
     0,
     {
         color: 'red',
         width: 3
     }
- );
- 
- var canvas = new Percept.Canvas(document.getElementById('canvas'));
- var drawing = new Percept.Drawing(canvas);
- 
- drawing.add(line);
- canvas.draw(drawing);
+);
+
+var drawing = new Percept.Drawing(canvas);
+
+drawing.add(line);
+canvas.draw(drawing);
