7543ab92e941ba673f670c375823da5109a9714a
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-24 23:44:16 +0530

    ex4 modify

--- examples/CreatingViews/Rectangle/rectangle.js
+++ examples/CreatingViews/Rectangle/rectangle.js
@@ -3,7 +3,7 @@ var canvas = new Percept.Canvas(document.getElementById('canvas'));
 var rect1 = new Percept.View.Rectangle(
     'rect1',
     new Percept.Vector2(canvas.width * .3, canvas.height / 2),
-    20, 20,
+    40, 40,
     {
         fill: true,
         fillColor: 'orange',
@@ -13,7 +13,7 @@ var rect1 = new Percept.View.Rectangle(
 var rect2 = new Percept.View.Rectangle(
     'rect2',
     new Percept.Vector2(canvas.width * .6, canvas.height / 2),
-    40, 40,
+    60, 60,
     {
         fill: true,
         outline: true,
