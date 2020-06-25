dec06eee99c543fe929481e193a62e4076e65f3c
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-25 21:44:59 +0530

    ex10

---
+++ examples/UpdatingViews/GlobalUpdate/globalUpdate.js
@@ -0,0 +1,23 @@
+var canvas = new Percept.Canvas(document.getElementById('canvas'));
+
+var rect1 = new Percept.View.Rectangle('rect1', Percept.Vector2.Random(canvas), 40, 40, {
+    fill: true,
+    fillColor: 'violet'
+});
+
+var rect2 = new Percept.View.Rectangle('rect2', Percept.Vector2.Random(canvas), 20, 20, {
+    fill: true,
+    fillColor: 'orange'
+});
+
+var rect3 = new Percept.View.Rectangle('rect3', Percept.Vector2.Random(canvas), 60, 60, {
+    fill: true,
+    fillColor: 'lime'
+});
+
+var drawing = new Percept.Drawing(canvas, () => {
+    rect1.localRotation += .2;
+    rect2.localRotation += 1;
+    rect3.localRotation += .5;
+});
+canvas.draw(drawing);
