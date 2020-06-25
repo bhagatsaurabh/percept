6659dcfd6dbb0b0e66613502b2446219236605bd
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-25 17:45:53 +0530

    ex8 modify

--- examples/CreatingViews/Empty/empty.js
+++ examples/CreatingViews/Empty/empty.js
@@ -3,7 +3,7 @@ var drawing = new Percept.Drawing(canvas);
 
 for (var i = 0 ; i < 50 ; i++) {
     var empty = new Percept.View.Empty('empty' + i, new Percept.Vector2(canvas.width / 2, canvas.height / 2));
-    empty.on('update', function() {
+    empty.on('update', function(view) {
         Percept.Debug.debugPoint(this.id, drawing, this.absolutePosition, {color: 'blue'});
     });
 }
