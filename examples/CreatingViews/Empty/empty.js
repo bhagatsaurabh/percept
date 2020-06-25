aa2fa7eda98536edbeeb2e33e6c6cc9c11815693
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-25 17:40:00 +0530

    ex8 modify

--- examples/CreatingViews/Empty/empty.js
+++ examples/CreatingViews/Empty/empty.js
@@ -1,9 +1,12 @@
 var canvas = new Percept.Canvas(document.getElementById('canvas'));
+var drawing = new Percept.Drawing(canvas);
 
-var empty = new Percept.View.Empty('empty', new Percept.Vector2(canvas.width / 2, canvas.height / 2));
+for (var i = 0 ; i < 50 ; i++) {
+    var empty = new Percept.View.Empty('empty' + i, new Percept.Vector2(canvas.width / 2, canvas.height / 2));
+    empty.on('update', function() {
+        Percept.Debug.debugPoint(this.id, drawing, this.absolutePosition, {color: 'blue'});
+    });
+}
 
-var drawing = new Percept.Drawing(canvas, () => {
-    Percept.Debug.debugPoint('emptyView', drawing, empty.absolutePosition, {color: 'blue'});
-});
 drawing.add(empty);
 canvas.draw(drawing);
