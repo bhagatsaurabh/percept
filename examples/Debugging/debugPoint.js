2f5064119041d8f43d31c488b2a9aa46b3fd7c7e
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-06-25 22:34:44 +0530

    ex11 modify

--- examples/Debugging/debugPoint.js
+++ examples/Debugging/debugPoint.js
@@ -18,6 +18,8 @@ var poly1 = new Percept.View.Polygon('poly1',
 poly1.position = new Percept.Vector2(canvas.width / 2, canvas.height / 2);
 
 var drawing = new Percept.Drawing(canvas, () => {
+    poly1.localRotation += 1;
+
     Percept.Debug.debugPoint('polyCenter', drawing, poly1.absolutePosition, {color: 'blue', radius: 3});
 });
 
